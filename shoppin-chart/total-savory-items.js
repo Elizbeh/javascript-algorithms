1fc26120ed365766726db9b155631071eaf36dd6
Elizbeh <105496336+Elizbeh@users.noreply.github.com>
2026-01-09 11:10:30 +0100

    add array filtering, mapping, and sorting practice

---
+++ shoppin-chart/total-savory-items.js
@@ -0,0 +1,21 @@
+import shoppingCart from "./data.js";
+
+/*
+Use reduce() and only reduce() to calculate and return 
+the total cost of only the savory
+items in the shopping cart.
+
+Expected output: 9.97  
+*/
+
+function totalSavory(arr){
+    return arr.reduce((acc, curr) => {
+        if (curr.type === "savory") {
+            return acc += curr.price
+        }
+        return acc
+    }, 0)
+}
+
+console.log(totalSavory(shoppingCart));
+
